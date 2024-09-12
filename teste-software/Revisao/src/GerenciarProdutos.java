38750cd45e1359c58dc82d1df6dd3953419bf9cb
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2024-09-12 10:46:30 -0300

    Construindo a func cadastrar

--- teste-software/Revisao/src/GerenciarProdutos.java
+++ teste-software/Revisao/src/GerenciarProdutos.java
@@ -1,63 +1,87 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import model.Disponibilidade;
 import model.Produto;
 
 public class GerenciarProdutos {
- 
-    private List<Produto> produtos;
-    private Scanner sc;
+
+    private List<Produto> produtos = new ArrayList<>(); // Cria a lista de produtos
+    private Scanner sc = new Scanner(System.in); // Cria o Scanner
+
     public static void main(String[] args) {
-        
-        GerenciarProdutos gp = new GerenciarProdutos();
 
-        gp.produtos = new ArrayList<>(); //Cria a lista de produtos
-        gp.sc = new Scanner(System.in);
+        GerenciarProdutos gp = new GerenciarProdutos();// Cria o objeto da classe
 
-        //Loop de menu
+        // Loop de menu
         int opc = 0;
-        do{
+        do {
             gp.menu();
             System.out.println("Escolha a operação: ");
             opc = Integer.parseInt(gp.sc.nextLine());
-            
+
             switch (opc) {
                 case 1:
-                    gp.execCadastrar();
+                    String nome;
+                    double preco;
+                    String disponibilidade;
+                    LocalDate dataCastro;
+                    System.out.println("============Cadastrando novo produto======");
+                    System.out.println("Digite o nome do produto");
+                    nome = gp.sc.nextLine();
+
+                    System.out.println("\n Digite o preco do produto");
+                    preco = Double.parseDouble(gp.sc.nextLine());
+
+                    System.out.println("Qual a disponibilidade do produto: ");
+                    disponibilidade = gp.sc.nextLine().toUpperCase();
+
+                    dataCastro = LocalDate.now();
+
                     break;
-            
+
                 case 4:
                     gp.execSair();
-                break;
+                    break;
                 default:
                     break;
             }
 
-
-        }while( opc !=4);
-
-    }
-
-    //Metodos
-   private void execCadastrar(){
+        } while (opc != 4);
 
     }
 
-    private void exeConsultar(){
+    // Metodos
+    private void execCadastrar(String nome, double preco, String disponibilidade, LocalDate dataCastro) {
+        Disponibilidade dis =  new Disponibilidade();
+        dis.valueOf(disponibilidade);
+
+        Produto produto = new Produto(nome, preco, dis, dataCastro);
+        produtos.add(produto);
 
     }
-   private void exeConsultarPornome(){
+
+    private void exeConsultar() {
 
     }
-   private void execAlterarDisponibilidade(){
+
+    private void exeConsultarPornome() {
 
     }
-    private void execSair(){
+
+    private void execAlterarDisponibilidade() {
+
+    }
+
+    private void execSair() {
         System.out.println("Saindo do sistema");
     }
 
-    private void menu(){
+    private void menu() {
         System.out.println("=========Menu==========");
         System.out.println("1. Cadastrar novo Produto");
         System.out.println("2. Listar produtos cadastrador");
